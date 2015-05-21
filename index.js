faebbd01f750409c3c429c47a75c584712cc2625
nathanallen <thenathanator@gmail.com>
2015-05-21 14:21:43 -0700

    better examples

--- index.js
+++ index.js
@@ -1,44 +1,63 @@
-// Simple API Integration Demo
-// We'll use the "request" library to handle external API requests
-// https://github.com/request/request
-
 var express = require('express'),
     app = express(),
     request = require('request');
 
-// When a user navigates to '/reddit' in the browser (on the FRONTEND)
-// we'll make an api request to reddit...            (on the BACKEND)
-// When that api request gets back to us             (on the BACKEND)
-// we'll send the api data over to the user          (on the FRONTEND)
-// but you could also save it to a database, or parse it, or mash it up w/ other data!
-
 // REDDIT EXAMPLE
 app.get('/reddit', function(req, res){
 
+  console.log("Requesting data from reddit...")
   request.get({
-    uri: "http://www.reddit.com/r/funny.json"
-  }, function(err, data){
-    // okay, the reddit api data is back!
-    res.send(data); // Let's hand it over to the frontend!
+    uri: "http://www.reddit.com/.json"
+  }, function(err, apiRes, apiBody){
+      
+      if (err) {
+        console.log("Uh oh! Got an error from reddit.")
+        res.send("There was an error")
+      }
+
+      console.log("Reddit api response is back!")
+      var responseData = JSON.parse(apiBody);
+
+      console.log("checking for AMA on the front page...")
+      var frontPageHasAMA = responseData.data.children.some(function(item){
+        return item.data.subreddit === "IAmA";
+      });
+
+      if (frontPageHasAMA) {
+        console.log("Found one!")
+        res.send("Yay! There's an AMA on the front page of <a href='http://www.reddit.com'>reddit</a> today! :)")
+      } else {
+        console.log("Nope, nothing!")
+        res.send("Sorry! No AMA today... :(");
+      }
+      
   })
 
 })
 
-// GIPHY EXAMPLE
+// GIPHY "proxy" EXAMPLE
 app.get('/giphy', function(req, res){
   
+  console.log("Requesting data from giphy...")
   request.get({
-    uri: "http://api.giphy.com/v1/gifs/search",
-    // this time we have a query string
+    uri: "http://api.giphy.com/v1/gifs/trending",
     qs: {
-      q: "funny+cat",
+      limit: 1,
       api_key: "dc6zaTOxFJmzC"
     }
-  }, function(err, data){
-    // okay, the giphy api data is back!
-    // YOUR CODE HERE!
-    // Maybe save api data to the database? Or parse it?
-    res.send(data); // or send it on back!
+  }, function(err, apiRes, apiBody){
+    
+      if (err) {
+        console.log("Uh oh! Got an error from reddit.")
+        res.send("There was an error")
+      }
+
+      console.log("Giphy api response is back!")
+      var body = JSON.parse(apiRes.body)
+
+      console.log("Grabbing the top trending gif")
+      res.send("<img src='" + body.data[0].images.original.url + "'>");
+
   })
 
 })
